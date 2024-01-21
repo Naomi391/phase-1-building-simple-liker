308d023d6ec2d0d66d1f3a2fd189cd7416e6bc5d
Naomi Mogi <nyabokenaomi001@gmail.com>
2024-01-21 13:44:37 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -1,20 +1,48 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = "♡";
+const FULL_HEART = "♥";
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  const EMPTY_HEART = "♡";
+  const FULL_HEART = "♥";
 
+  const errorModal = document.getElementById("modal");
+  const hearts = document.querySelectorAll(".like-glyph");
 
+  errorModal.classList.add("hidden");
 
+  hearts.forEach((heart) => {
+    heart.addEventListener("click", () => {
+      mimicServerCall()
+        .then(() => {
+          // Corrected typo: innerTEXT to innerText
+          heart.innerText = FULL_HEART;
+          // Corrected class name: activatedHeart to activated-heart
+          heart.classList.add("activated-heart");
+        })
+        .catch((error) => {
+          errorModal.classList.remove("hidden");
+
+          const errorMessage = document.getElementById("modal-message");
+          errorMessage.innerText = error;
+
+          setTimeout(() => {
+            errorModal.classList.add("hidden");
+          }, 3000);
+        });
+    });
+  });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isRandomFailure = Math.random() < 0.2;
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
@@ -23,3 +51,4 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+mimicServerCall();
